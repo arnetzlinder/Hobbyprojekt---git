12710f8722807e47ed95902b3f8b4d6a5c060ccc
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-21 22:39:25 +0100

    added functionality to views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,12 @@
-
+<script lang="ts">
+import AudioPlay from "../components/AudioPlay.vue"
+export default {
+    name: "AudioPlay",
+    components: {
+        "AudioPlay":AudioPlay
+    }
+}
+</script>
 
 <template>
     <div>Home</div>

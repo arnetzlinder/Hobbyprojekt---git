625725fcc36a9fa603d41c17b5823a7b92dce993
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-21 08:44:02 +0100

    add buttonmenu and content to menu

---
+++ src/components/ButtonMenu.vue
@@ -0,0 +1,64 @@
+<template>
+    <nav id="navbar" class="drop-shadow-2xl bg-gradient-to-r from-red-400 via-orange-100 to-yellow-100">
+        <ul>
+        <li><a href="#" target="_blank"><p>Lyssna på mig</p></a></li>
+        <li><a href="#" target="_blank"><p>Om mig</p></a></li>
+        <li><a href="#" target="_blank"><p>Kontakt</p></a></li>
+        </ul>
+        </nav>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped lang="scss">
+#navbar {
+  margin: 0;
+  margin-top: 5rem;
+  min-height: 23rem;
+  width: 22rem;
+  max-width: 50rem;
+  border-radius: 15px;
+  position: absolute;
+  right: 1rem; 
+  text-align: center;
+  
+  li {
+    list-style: none;
+    line-height: 3rem;
+  }
+  a {
+    display: inline;
+  }
+  
+}
+
+
+@media (width >= 1440px) {
+    #navbar {
+  margin: 0;
+  margin-top: 5rem;
+//   min-height: 23rem;
+  width: 50rem;
+  position: absolute;
+  border-radius: 15px;
+  font-size: 2rem;
+  line-height: 2rem;
+//   right: 28rem; 
+  text-align: center;
+  
+  li {
+    padding-top: 2rem;
+    list-style: none;
+    line-height: 3rem;
+  }
+  a {
+    display: inline;
+  }
+  
+}
+
+}
+  
+</style>
---
+++ src/components/MenuButton.vue
@@ -0,0 +1,178 @@
+<template>
+    <div id="burger" :class="{ active: isOpen }" @click="onToggle" :inheritAttrs="false">
+      <button type="button" class="burger-button" title="Menu">
+        <span class="burger-bar burger-bar--1"></span>
+        <span class="burger-bar burger-bar--2"></span>
+        <span class="burger-bar burger-bar--3"></span>
+      </button>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+     props: {
+       isOpen: Boolean
+     },
+     methods: {
+       onToggle() {
+         // this.$emit('click');
+       }
+     }
+   };
+  </script>
+  
+  <style scoped lang="scss">
+  button {
+    cursor: pointer;
+    &:active,
+    &:focus {
+      outline: 0;
+    }
+  }
+
+  .burger-button {
+    position: relative;
+    height: 35px;
+    width: 37px;
+    right: 0;
+    display: block;
+    z-index: 10;
+    border: 0;
+    border-radius: 0;
+    background-color: transparent;
+    pointer-events: all;
+    transition: transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+  .burger-bar {
+    background-color: greenyellow;
+    position: absolute;
+    top: 50%;
+    right: 6px;
+    left: 6px;
+    height: 2px;
+    width: auto;
+    margin-top: -1px;
+    border-color: greenyellow;
+    transition: transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1), opacity 0.3s cubic-bezier(0.165, 0.84, 0.44, 1),
+      background-color 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+  .burger-bar--1 {
+    -webkit-transform: translateY(-6px);
+    transform: translateY(-6px);
+  }
+  .burger-bar--2 {
+    transform-origin: 100% 50%;
+    transform: scaleX(0.8);
+  }
+  .burger-button:hover .burger-bar--2 {
+    transform: scaleX(1);
+  }
+  .no-touchevents .burger-bar--2:hover {
+    transform: scaleX(1);
+  }
+  .burger-bar--3 {
+    transform: translateY(6px);
+  }
+  #burger.active .burger-button {
+    transform: rotate(-180deg);
+  }
+  #burger.active .burger-bar--1 {
+    transform: rotate(45deg);
+  }
+  #burger.active .burger-bar--2 {
+    opacity: 0;
+  }
+  #burger.active .burger-bar--3 {
+    transform: rotate(-45deg);
+  }
+  @media (width >= 740px) {
+  .burger-button {
+    position: relative;
+    height: 60px;
+    width: 60px;
+    right: -120px;
+    top: -10px;
+    display: block;
+    z-index: 10;
+    border: 0;
+    border-radius: 0;
+    background-color: transparent;
+    pointer-events: all;
+    transition: transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+  .burger-bar {
+    background-color: greenyellow;
+    position: absolute;
+    top: 50%;
+    right: 6px;
+    left: 6px;
+    height: 2px;
+    width: auto;
+    margin-top: -1px;
+    border-color: greenyellow;
+    transition: transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1), opacity 0.3s cubic-bezier(0.165, 0.84, 0.44, 1),
+      background-color 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+
+}
+ @media (width >= 740px) {
+  .burger-button {
+    position: relative;
+    height: 60px;
+    width: 60px;
+    right: -120px;
+    top: -10px;
+    display: block;
+    z-index: 10;
+    border: 0;
+    border-radius: 0;
+    background-color: transparent;
+    pointer-events: all;
+    transition: transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+  .burger-bar {
+    background-color: greenyellow;
+    position: absolute;
+    top: 50%;
+    right: 6px;
+    left: 6px;
+    height: 2px;
+    width: auto;
+    margin-top: -1px;
+    border-color: greenyellow;
+    transition: transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1), opacity 0.3s cubic-bezier(0.165, 0.84, 0.44, 1),
+      background-color 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+
+}
+@media (width >= 1440px) {
+  .burger-button {
+    position: relative;
+    height: 70px;
+    width: 70px;
+    right: -220px;
+    top: -10px;
+    display: block;
+    z-index: 10;
+    border: 0;
+    border-radius: 0;
+    background-color: transparent;
+    pointer-events: all;
+    transition: transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+  .burger-bar {
+    background-color: greenyellow;
+    position: absolute;
+    top: 50%;
+    right: 6px;
+    left: 6px;
+    height: 2px;
+    width: auto;
+    margin-top: -1px;
+    border-color: greenyellow;
+    transition: transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1), opacity 0.3s cubic-bezier(0.165, 0.84, 0.44, 1),
+      background-color 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+
+}
+  </style>

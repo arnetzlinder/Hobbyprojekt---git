ace11b5b3860b5680ffc58451851240ce37c0e0c
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-21 13:49:38 +0100

    :lipstick:added styling

--- src/components/AudioPlay.vue
+++ src/components/AudioPlay.vue
@@ -5,36 +5,36 @@ export default{};
 </script>
 
 <template>
-    <div class="from-green-200 via-yellow-100 to-blue-400 text-gray-800">
-        <div class="from-green-200 via-yellow-100 to-blue-400 text-gray-800" id="audio-player-root"></div>
+    <div class="text-gray-800 p-20 h-screen flex relative">
 
-        <div class="from-green-200 via-yellow-100 to-blue-400 text-gray-800">
-            <label  class="from-green-200 via-yellow-100 to-blue-400 text-gray-800">
+
+        <div class="flex  flex-col text-gray-800 content-evenly">
+            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 p-10">
             <audio controls>
                 <source src="/Hallelujah.mp3" type="audio/mpeg">
             </audio>
-            <p class="ml-20">Hallelujah</p>
+            <p class="ml-10">Hallelujah</p>
             </label>
             <br>
-            <label>
+            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 p-10">
             <audio controls>
                 <source src="/Blott en dag.mp3" type="audio/mpeg">
             </audio>
-            <p class="ml-20">Blott en dag</p>
+            <p class="ml-10">Blott en dag</p>
             </label>
             <br>
-            <label>
+            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 p-10">
             <audio controls>
                 <source src="/För kärlekens skull.mp3" type="audio/mpeg">
             </audio>
-            <p class="ml-20">För kärlekens skull</p>
+            <p class="ml-10">För kärlekens skull</p>
             </label>
             <br>
-            <label>
+            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 p-10">
             <audio controls>
                 <source src="/Prins Noel.mp3" type="audio/mpeg">
             </audio>
-            <p class="ml-20">Prins Noel</p>
+            <p class="ml-10">Prins Noel</p>
             </label>
         </div>
 

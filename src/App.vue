10673513c11e11f132e2a6ab1d571163b0113dcf
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-21 21:28:07 +0100

    added multiple pages to router-view

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,14 @@ console.log("för rebase");
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  
+  <RouterLink to="./">Home</RouterLink>
+  <RouterLink to="./about">About</RouterLink>
+  <router-view></router-view>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,16 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path:'/'
+            path:'/',
             name: 'home',
-            component: () => import('../views/HomeView.vue') )
+            component: () => import('../views/HomeView.vue'),
+        },
+        {
+            path:'/about',
+            name: 'about',
+            component: () => import('../views/AboutMe.vue'),
         }
     ],
 });
+
+export default router;

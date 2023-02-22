154c7998dfb15e1d927a12dcf9588d9a0062a0aa
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-22 22:31:52 +0100

    :lipstick: add some styling

--- src/views/ContactMe.vue
+++ src/views/ContactMe.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="m-20 rounded-3xl mt-20 text-2xl max-h-96 max-w-md font-serif absolute justify-center content-center flex flex-col bg-gradient-to-r from-green-200 via-yellow-100 to-blue-400">
-        <h1 class="uppercase text-3xl p-10"> Kontakta mig</h1>
-        <p class="p-10 font-serif"> Denna sida är gjord som ett projekt i min nuvarande utbildning till Frontend-utvecklare. För kontakt, se nedan: </p>
+    <div class="flex flex-col md:flex-row lg:flex-row relative">
+        <div class="rounded-3xl mt-20 text-2xl max-h-96 max-w-md font-serif absolute justify-center content-center flex flex-col bg-gradient-to-r from-green-200 via-yellow-100 to-blue-400">
+            <h1 class="uppercase text-3xl p-10"> Kontakta mig</h1>
+            <p class="p-10 font-serif"> Denna sida är gjord som ett projekt i min nuvarande utbildning till Frontend-utvecklare. För kontakt, se nedan: </p>
+            <br>
+            <br>
+            <ul class="flex flex-row content-around">
+                <li class="p-10"> <span class="location"> <i class="fa-solid fa-location-dot"></i></span> Stockholm</li>
+                <li class="p-10"> <a href="https://github.com/arnetzlinder"><span class="github"> <i class="fa-brands fa-github"></i></span> </a></li>
+                <li class="p-10"> <a href="https://www.linkedin.com/in/susanne-arnetz-linder-999a2481/"><span class="linkedIn"> <i class="fa-brands fa-linkedin"></i></span> </a></li>
+            </ul>
+        </div> 
         <br>
-        <br>
-        <ul class="flex flex-row content-around">
-            <li class="p-10"> <span class="location"> <i class="fa-solid fa-location-dot"></i></span> Stockholm</li>
-            <li class="p-10"> <a href="https://github.com/arnetzlinder"><span class="github"> <i class="fa-brands fa-github"></i></span> </a></li>
-            <li class="p-10"> <a href="https://www.linkedin.com/in/susanne-arnetz-linder-999a2481/"><span class="linkedIn"> <i class="fa-brands fa-linkedin"></i></span> </a></li>
-    </ul>
-</div> 
+        <div class="rounded-3xl text-2xl max-h-96 max-w-md font-serif relative bg-gradient-to-r from-green-200 via-yellow-100 to-blue-400">
+        <h2 class="uppercase text-3xl p-10"> Referenser</h2>
+        <p> En klar, ren stämma som verkligen gjorde begravningen så bra den kunde bli, under de omständigheterna</p>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">

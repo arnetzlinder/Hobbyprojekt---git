165f72f3a52e856e2e67d8d005423e9a16d12d31
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-22 08:47:20 +0100

    added contact and menulinks

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,7 @@ import SongNavbar from './components/SongNavbar.vue';
 
   <div class="pt-20 z-20 relative">
   
-  <RouterLink to="./">Startsida</RouterLink>
-  <RouterLink to="./about">Om mig</RouterLink>
-  <RouterLink to="./contact">Kontakt</RouterLink>
+
   <router-view></router-view>
 </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,5 +9,7 @@ export default {
 </script>
 
 <template>
-    <div>Home</div>
+    <div>
+        <!-- <AudioPlay /> -->
+    </div>
 </template>

51f7de2e51df27e40ccc5e0d65a9587f8d560d40
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-23 11:19:48 +0100

    mediaquerys

--- src/components/AudioPlay.vue
+++ src/components/AudioPlay.vue
@@ -5,32 +5,32 @@ export default{};
 </script>
 
 <template>
-    <div class="text-gray-800 p-5 md:p-10 lg:p-20 flex relative">
+    <div class="text-gray-800 p-5 md:p-10 lg:p-20 flex relative max-w-sm md:max-w-lg lg:max-w-xl">
 
 
         <div class="bg-gradient-to-r from-blue-400 via-yellow-100 to-green-200 flex  flex-col text-gray-800 content-evenly border-8 border-gray-800 rounded-xl">
-            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 p-10">
+            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 pt-10 pb-10 mx-1">
             <audio controls>
                 <source src="/Hallelujah.mp3" type="audio/mpeg">
             </audio>
             <p class="ml-10">Hallelujah</p>
             </label>
             <br>
-            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 p-10">
+            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 pt-10 pb-10 mx-1">
             <audio controls>
                 <source src="/Blott en dag.mp3" type="audio/mpeg">
             </audio>
             <p class="ml-10">Blott en dag</p>
             </label>
             <br>
-            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 p-10">
+            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 pt-10 pb-10 mx-1">
             <audio controls>
                 <source src="/För kärlekens skull.mp3" type="audio/mpeg">
             </audio>
             <p class="ml-10">För kärlekens skull</p>
             </label>
             <br>
-            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 p-10">
+            <label class="text-gray-800 md:hover:scale-125 lg:hover:scale-125 pt-10 pb-10 mx-1">
             <audio controls>
                 <source src="/Prins Noel.mp3" type="audio/mpeg">
             </audio>
--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,10 +1,8 @@
 <template>
-    <div class="max-h-96 font-serif relative justify-center content-center flex">
-        <div class="absolute p-10 text-2xl bg-gradient-to-r from-green-200 via-yellow-100 to-blue-400 content-evenly rounded-xl">
-            <h1 class="font-bold text-3xl pb-5"> Om mig</h1>
-            <p class="max-w-2xl">Jag är en sångerska med gedigen erfarenhet av att sjunga, jag har sjungit i kör sedan fyra års ålder, gått i musikklass och på sång i den kommunala musikskolan. Utöver det har jag även tagit lektioner privat genom åren.</p> 
-            <br>
-            <p class="max-w-2xl">Numera sjunger jag förstasopran i en vokalensamble som heter Ars Longa även om jag för tillfället har uppehåll då jag har en liten hemma. Yrkesmässigt är jag utbildad distriktssköterska som bestämt mig för att sadla om 
+    <div class="max-h-96 font-serif top-20 relative justify-center content-center flex">
+        <div class="absolute p-10 text-xl bg-gradient-to-r from-green-200 via-yellow-100 to-blue-400 content-evenly rounded-xl">
+            <h1 class="font-bold text-2xl pb-5"> Om mig</h1>
+            <p class="max-w-2xl"> Jag är en sångerska med gedigen erfarenhet av att sjunga, både solo och i kör. Numera sjunger jag förstasopran i en vokalensamble som heter Ars Longa. Yrkesmässigt är jag utbildad distriktssköterska som bestämt mig för att sadla om 
             och numera läser till Frontend-utvecklare vid Medieinstitutet.</p>
             <br>
             <ul class="list-disc">
@@ -16,7 +14,7 @@
             <h2 class="font-bold mt-10 mb-2">Kuriosa</h2>
             <p class="max-w-2xl">2013 spelade jag in en skiva med min dåvarande kör Ascalpella, Medicinska föreningens kammarkör. Jag sjöng solot på låten nedan på litauiska, lyssna gärna!</p>
             <br>
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1HarcPWV5J9xCpNppJhT3V?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1HarcPWV5J9xCpNppJhT3V?utm_source=generator&theme=0" width="100%" height="252" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div>
     </div>
 </template>

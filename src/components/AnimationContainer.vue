ab14453c6a2b6a7f874a8f6aa1045629c222be7b
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-23 08:30:35 +0100

    add animation, needs bug fix

---
+++ src/components/AnimationContainer.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { gsap } from "gsap";
+    import MusicAnimation from "./MusicAnimation.vue";
+    export default {
+        name: "NotesAnimation",
+        components: {
+            MusicAnimation,
+        },
+        data() {
+            return {};
+        },
+        mounted() {
+            
+        }
+    }
+</script>
